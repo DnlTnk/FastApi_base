8d747df35cda0ba2f580518c0d575d53b4c1bda6
Danilo Tanaka <danilotanaka@Danilos-MacBook-Air.local>
2023-03-28 13:17:42 -0300

    Adding debugpy to debug inside container.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+import debugpy
+debugpy.listen(("0.0.0.0", 5678))
+
 @app.get("/")
 def read_root():
-    return{"Hello": "World"}
+    return{"Hello": "World 23"}
